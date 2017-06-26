3045f5572acff427c2f270ed078a0d18d538761e
BoCongChen <onion1120@hotmail.com.tw>
2017-06-26 13:19:22 +0800

    Add files via upload

--- Tool/netgenerate.m
+++ Tool/netgenerate.m
@@ -12,9 +12,32 @@ switch property
         adj = floor(rand(N)+p) ;
         adj = adj - diag(diag(adj)) ;
     case 'rand_2'
-        p   =  varargin{1};
+        p   = varargin{1} ;
         adj = floor(triu(rand(N)+p,1)) ;
         adj = adj + adj';
+    case 'BA_model'
+        adj = false(N) ;
+        adj([3 N+3 2*N+1 2*N+2]) = true ;
+        v = 3 ;
+        while v < N
+            d = sum(adj,2) ;
+            p = d/v ;
+            for link = 1 : 2
+                i = 0 ;
+                number = rand*sum(p) ;
+                while number > 0
+                    i = i + 1 ;
+                    number = number - p(i) ;
+                end
+                adj(i,v+1) = true ; adj(v+1,i) = true ;
+                p(i) = 0 ;
+            end
+            v = v + 1 ;
+        end
+    case 'radiation'
+        adj = false(N) ;
+        adj(1,2:end) = true ;
+        adj(2:end,1) = true ;
     case '1-dir'
         adj = circshift(eye(N),[1 0]) ;
         adj(1,end) = 0 ;
@@ -23,15 +46,22 @@ switch property
     case 'square'
         x = N(1) ;
         y = N(2) ;
-        adj = zeros(x*y) ;
-        B = reshape(1:x*y,[x,y]) ;
+        adj = false(x*y) ;
+        B = reshape(1:x*y,[x,y])' ;
         C = [B B B ; B B B ; B B B] ;
         for n = 1 : x*y
-            i = rem(n,x)  + x ;
-            j = ceil(n/x) + y ;
+            i = ceil(n/x) + y ;
+            j = rem(n,x)  + x ;
             v = [C(i+1,j) C(i,j+1) C(i-1,j) C(i,j-1)] ;
-            adj(v,n) = 1 ;
+            adj(v,n) = true ;
         end
+end
+
+switch length(adj) > 70
+    case 0
+        adj = logical(adj) ;
+    case 1
+        adj = sparse(adj) ;
 end
 
 end
